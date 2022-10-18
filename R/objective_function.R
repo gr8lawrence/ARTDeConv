04be53923d47a0fa8b3145a3fb57aaca7b7f2381
gr8lawrence <gr8lawrence@gmail.com>
2022-10-18 12:23:45 -0400

    Fixed one typo in the objective function

--- R/objective_function.R
+++ R/objective_function.R
@@ -2,6 +2,6 @@
 
 artdeconv_obj_fun <- function(Y, Y_hat, Theta_hat, P_hat, m, n, k, m0, k0, Theta_0, meds, ranges, alpha_1, alpha_2, beta) {
   Delta <- get_Delta(k, k0, m, m0)
-  return(1/(2 * m * n) * norm(Y - Y_hat, type = 'F')^2 + 1/2 * alpha_1 * norm(Delta * Theta_hat - Theta_0, type = 'F')^2 + 
-    alpha_2 * norm((1 - Delta) * Theta_hat, type = 'F')^2 + beta * norm((1/ranges) *(P_hat - meds), type = 'F')^2)
+  return(1/(2 * m * n) * norm(Y - Y_hat, type = 'F')^2 + (1/2) * alpha_1 * norm(Delta * Theta_hat - Theta_0, type = 'F')^2 + 
+           alpha_2 * norm((1 - Delta) * Theta_hat, type = 'F')^2 + (1/2) * beta * norm((1/ranges) *(P_hat - meds), type = 'F')^2)
 }
