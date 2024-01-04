0967dbb58203b74ad506f0b3a1ed08fefdbbc21a
gr8lawrence <gr8lawrence@gmail.com>
2024-01-03 22:11:34 -0500

    Updated more documentations

--- src/ARTdeConv_cpp_funs.cpp
+++ src/ARTdeConv_cpp_funs.cpp
@@ -130,31 +130,31 @@ double obj_fun_cpp(const arma::mat Y, const arma::mat Y_hat, const arma::mat The
 //' initial values in the input. Otherwise, the required parameters are the same as the main ARTdeConv function
 //' with restarts.
 //'
- //' @param Y The bulk matrix.
- //' @param Theta_0 The \eqn{m_0 \times K} reference signature matrix. The last \eqn{K_0} columns for cell types with unknown reference gene expression should be padded with 0s. 
- //' @param Theta_it The initial value of Theta.
- //' @param P_it The initial value of P.
- //' @param m0 The number of gene features we have prior knowledge about in `Theta_0`. 
- //' @param k0 The number of cell types whose cell-type level expression we have prior knowledge about in `Theta_0`.
- //' @param meds The vector of median cell proportions in the population.
- //' @param ranges The vector of the range interval lengths of proportions of different cell types.
- //' @param alpha1 The tuning parameter for regularizing the part of Theta with prior knowledge.
- //' @param alpha2 The tuning parameter for regularizing the part of Theta without prior knowledge.
- //' @param beta The tuning parameter for regularizing P.
- //' @param max_iter The maximal number of iterations this core function will run. The default is `1e5`.
- //' @param tol The tolerance parameter for the convergence criterion of ARTdeConv. The default is `1e-5`.
- //'
- //' @return A list with the following items:
- //' * Y_hat: the estimated bulk expression from deconvolution; 
- //' * Theta_hat: the estimated signature matrix from deconvolution;
- //' * s_hat: the vector of estimated mRNA amounts;
- //' * P_hat: the estimated proportion (the results of primary interests);
- //' * res_v: the vector of bulk expression residuals after each iteration;
- //' * obj_v: the vector of objective function values after each iteration;
- //' * fixed_s: the indicator of whether the mRNA amount vector is fixed to be 1 for all cell types;
- //' * weights: the indicator of whether adaptive penalization is used;
- //' * n_iter: the number of iterations the algorithm has run.
- //' 
+//' @param Y The bulk matrix.
+//' @param Theta_0 The \eqn{m \times K} reference signature matrix. The last \eqn{K_0} columns for cell types with unknown reference gene expression should be padded with 0s. 
+//' @param Theta_it The initial value of Theta.
+//' @param P_it The initial value of P.
+//' @param m0 The number of gene features we have prior knowledge about in `Theta_0`. 
+//' @param k0 The number of cell types whose cell-type level expression we have prior knowledge about in `Theta_0`.
+//' @param meds The vector of median cell proportions in the population.
+//' @param ranges The vector of the range interval lengths of proportions of different cell types.
+//' @param alpha1 The tuning parameter for regularizing the part of Theta with prior knowledge.
+//' @param alpha2 The tuning parameter for regularizing the part of Theta without prior knowledge.
+//' @param beta The tuning parameter for regularizing P.
+//' @param max_iter The maximal number of iterations this core function will run. The default is `1e5`.
+//' @param tol The tolerance parameter for the convergence criterion of ARTdeConv. The default is `1e-5`.
+//'
+//' @return A list with the following items:
+//' * Y_hat: the estimated bulk expression from deconvolution; 
+//' * Theta_hat: the estimated signature matrix from deconvolution;
+//' * s_hat: the vector of estimated mRNA amounts;
+//' * P_hat: the estimated proportion (the results of primary interests);
+//' * res_v: the vector of bulk expression residuals after each iteration;
+//' * obj_v: the vector of objective function values after each iteration;
+//' * fixed_s: the indicator of whether the mRNA amount vector is fixed to be 1 for all cell types;
+//' * weights: the indicator of whether adaptive penalization is used;
+//' * n_iter: the number of iterations the algorithm has run.
+//' 
 //' @export
 // [[Rcpp::export]]
 Rcpp::List artdeconv_single_solve_cpp(const arma::mat Y, const arma::mat Theta_0, const arma::mat Theta_it, const arma::vec s_it, const arma::mat P_it, const int m0, const int k0, const arma::vec meds, const arma::vec ranges, const double alpha1, const double alpha2, const double beta, const int max_iter = 1e5, const double tol = 1e-5) {
@@ -267,7 +267,7 @@ Rcpp::List artdeconv_single_solve_cpp(const arma::mat Y, const arma::mat Theta_0
 //' with restarts.
 //'
 //' @param Y The bulk matrix.
-//' @param Theta_0 The \eqn{m_0 \times K} reference signature matrix. The last \eqn{K_0} columns for cell types with unknown reference gene expression should be padded with 0s. 
+//' @param Theta_0 The \eqn{m \times K} reference signature matrix. The last \eqn{K_0} columns for cell types with unknown reference gene expression should be padded with 0s. 
 //' @param Theta_it The initial value of Theta.
 //' @param P_it The initial value of P.
 //' @param m0 The number of gene features we have prior knowledge about in `Theta_0`. 
